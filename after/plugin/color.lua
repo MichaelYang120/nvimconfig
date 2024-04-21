13748cb7d16e92b19ec6ff290c9a2fd1baa58d07
MichaelYang120 <michaelyangiscoding@gmail.com>
2024-04-21 01:49:34 -0500

    commented out unused lines

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,7 +1,7 @@
-require('rose-pine').setup({
-    disable_background = true
-})
-
+--require('rose-pine').setup({
+--    disable_background = true
+--})
+--
 function ColorMyPencils(color) 
     color = color or "rose-pine"
 	vim.cmd.colorscheme(color)
